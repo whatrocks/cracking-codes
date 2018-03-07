29d6406b09b9ab93274f17231dc4f9eb5731c387
Charlie Harrington <charlie@floydhub.com>
2018-03-06 18:24:17 -0800

    chapter 3

---
+++ 3.py
@@ -0,0 +1,30 @@
+* Strings are little chunks of text
+* When you type a string, put it between two single quotes
+* The quotes are not part of the string value
+* Strings can have almost any keyboard character in them
+* The '' string has zero characters. It is the blank string or empty string.
+* String concatenation with + operator
+* Python knows what to do with the + operator because of the data types of the values
+* String replication with the * muliply operator
+* String concatenation and string replication show that operators in Python can do different tasks based on the data types of the values they operate on
+* Indexing can get a single character from a string. Python indices start at 0
+* When you enter an index that is too large for the string, Python displays an "index out of range" error.
+* Negative indicies start at the end of string and go backward. The index of the last character in a string is -1
+* Slice can get more than one character from a string. Slice evaluates to a string that begins at the first value and goes up to but does not include the second index value.
+* Unlike indexes, slicing never gives you an erro if you give too large an index for a string, it will just return the widest matching slice it can
+* If you omit the first index of a slice, Python will automatically use 0
+* If you omit the second index, PYthon will automatically use the rest of the string
+* Printing values with the print() function
+* A function has code inside it that performs a task.
+* Python comes with many different functions and can perform useful tasks for you.
+* To call a function means to execute the code inside the function
+* The values that are passed when a function is called are called the arguments
+* You can pass expressions to a function as an argument instead of a single vlaue
+* Printing escape cahracters- this a backslash character followed by another specific characterz* Quotes and double quotes are interchangeable but you can't mix them
+* A computer program is a lot like the recipe. do the first step, then the second, and so on until you reach the end. When the program follows instructions step by step, we call it the program execution, or just the execution.
+* The execution starts at the first line and generally moves downward, but it can skip around
+* Any text following a hash mark (#) is a comment. These are not for the computer, they are for you, the programmer. You can also use this to comment out code. This can be useful when you are trying to figure out why a program doesn't work, and you can just remove the hash mark later
+* A function is a like a mini-program inside your program. Functions are great because we only need to know what the function does, not how it does it.
+* You can take input from a user with input() function
+* When the program executes the last line, it stops. It will terminate and exit. All the variables are forgotten by the computer.
+* Remember, a computer only does exactly what you program it to do. Computers are dumb.
