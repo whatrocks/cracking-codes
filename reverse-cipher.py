6acde0a9a5e861d3c5185e28796ccaa3648a8052
Charlie Harrington <charlie@floydhub.com>
2018-03-06 18:48:06 -0800

    implement reverse-cipher

--- reverse-cipher.py
+++ reverse-cipher.py
@@ -1,11 +1,13 @@
 # Reverse Cipher
 
-message = 'Three cna keep a secret, if two of them are dead.'
+# message = 'Three cna keep a secret, if two of them are dead.'
+message = input('Enter message: ')
 translated = ''
 
 i = len(message) - 1
 while i >= 0:
     translated = translated + message[i]
+    print(f'i is {i}, message[i] is {message[i]}, translated is {translated}')
     i = i - 1
 
 print(translated)
